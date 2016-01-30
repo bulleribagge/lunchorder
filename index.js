92fdd2a2f1372f69822c921a12bf799d62feb34d
bulleribagge <get_get_martin@hotmail.com>
2016-01-30 17:44:19 +0100

    testing db

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express();
 var port = 3000;
 
 app.all('/derp', function(req, res){
    res.send('fleerp 2'); 
+});
+
+app.all('/db', function(req, res){
+   
+   
+   pg.connect(process.env.DATABASE_URL, function(err, client){
+      if(err) throw err;
+      console.log('Connected to postgres! Getting orders...');
+      
+      client
+      .query('SELECT * FROM order;')
+      .on('row', function(row){
+          console.log(JSON.stringify(row));
+      });
+   });
 });
 
 app.listen(process.env.PORT || port);
