8400dcc365201349ea9992c61a099441b7dcdc91
bulleribagge <get_get_martin@hotmail.com>
2016-02-05 18:54:42 +0100

    testing parameters

--- routes/lunchorder.js
+++ routes/lunchorder.js
@@ -3,7 +3,7 @@ var pg = require('pg');
 var router = express.Router();
 
 router.all('/', function(req, res){
-    console.log(req.params.text);
+    console.log(req.params('text'));
     res.send('flerp');
 });
 
