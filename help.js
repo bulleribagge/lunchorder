0447e168c266a897cfac35185f2784ec09c540fa
bulleribagge <get_get_martin@hotmail.com>
2016-03-23 22:33:52 +0100

    cleaned up help text some more

--- help.js
+++ help.js
@@ -1,20 +1,20 @@
 function Help() { }
 
-Help.prototype.placeOrderHelp = `*placeorder*: Places an order
+Help.prototype.placeOrderHelp = `\n*placeorder*: Places an order
                             \n*Usage*: placeorder -m "main dish" -d "drink" --so "side order" -s "sauce" -e "extra"
                             \n*Example*: placeorder -m "BBQ" --so "Pommes" -d "Pepsi" -s "Aioli" -e "Ingen lök"
                             \nAny values not supplied will be default according to:
                             \n-m = "BBQ" -d = "Pepsi" -s "Aioli" --so "Pommes"
                             \nIf you wish to change your order, just place a new one.`;
                             
-Help.prototype.getOrderHelp = `*getorder*: Gets your order
+Help.prototype.getOrderHelp = `\n*getorder*: Gets your order
                             \n*Usage*: getorder`
                             
-Help.prototype.cancelOrderHelp = `*cancelorder*: Cancels your order
+Help.prototype.cancelOrderHelp = `\n*cancelorder*: Cancels your order
                             \n*Usage*: cancelorder`
 
 Help.prototype.getHelp = function(){
-    return this.placeOrderHelp + '\n\n' + this.getOrderHelp + '\n\n' + this.cancelOrderHelp;
+    return this.placeOrderHelp + '\n' + this.getOrderHelp + '\n' + this.cancelOrderHelp;
 }
 
 module.exports = Help;
