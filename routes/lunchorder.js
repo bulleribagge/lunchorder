14a25b379757f2adfae4d586f2d68a96292865db
bulleribagge <bulleribagge@users.noreply.github.com>
2016-04-08 11:01:50 +0200

    testing reboot

--- routes/lunchorder.js
+++ routes/lunchorder.js
@@ -8,6 +8,7 @@ var Util = require('../util');
 var OrderController = require('../controllers/ordercontroller');
 
 router.all('/', function(req, res) {
+    
     var argsv;
 
     if (req.body.token != process.env.SLACK_TOKEN) {
