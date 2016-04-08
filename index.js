f64f0efe27480079088ebd9368d5535404ddd2f8
bulleribagge <bulleribagge@users.noreply.github.com>
2016-04-08 12:31:50 +0200

    added cors

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ var port = 3000;
 var routes = require('./routes/lunchorder');
 var Sequelize = require('sequelize');
 var Model = require('./model');
+var cors = require('cors');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
     extended: true
