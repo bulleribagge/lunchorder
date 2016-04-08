000ec58e789db6d86f1e2593e0000e19fa379029
bulleribagge <bulleribagge@users.noreply.github.com>
2016-04-08 11:20:07 +0200

    Added CORS

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', routes);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 Model.create(function(){
     app.listen(process.env.PORT || port);
     console.log('listening on port ' + port + ' ...');        
