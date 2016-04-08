47fc4bf9aa6775a4fa64417f63eb030b6961bcaf
bulleribagge <bulleribagge@users.noreply.github.com>
2016-04-08 11:22:19 +0200

    mecking with CORS

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ var routes = require('./routes/lunchorder');
 var Sequelize = require('sequelize');
 var Model = require('./model');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use('/', routes);
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/', routes);
 
 Model.create(function(){
     app.listen(process.env.PORT || port);
